773698f78f61d07c24f7ca9865d9972a59c20045
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-10 21:33:10 +0530

    2770 Leetcode

--- Dynamic Programming/2770_Maximum_Number_of_Jumps_to_Reach_the_Last_Index.java
+++ Dynamic Programming/2770_Maximum_Number_of_Jumps_to_Reach_the_Last_Index.java
@@ -1,3 +1,10 @@
+// Question
+// You are given a 0-indexed array nums of n integers and an integer target.
+//You are initially positioned at index 0. In one step, you can jump from index i to any index j such that:
+//0 <= i < j < n
+//-target <= nums[j] - nums[i] <= target
+// Return the maximum number of jumps you can make to reach index n - 1.
+// If there is no way to reach index n - 1, return -1.
 class Solution {
     public int maximumJumps(int[] nums, int target) {
         int n = nums.length;
@@ -13,3 +20,7 @@ class Solution {
         return dp[0];
     }
 }
+// Explanation: 
+// We first define a dp array where dp[i] stores the maximum number of jumps we can take to reach n-1 index. 
+// We iterate through the array backwards as overlapping subproblems occur for future indices which are now tabulated by dp array 
+// According to the constraints the jump can be performed only when difference between values at indices is less than target
