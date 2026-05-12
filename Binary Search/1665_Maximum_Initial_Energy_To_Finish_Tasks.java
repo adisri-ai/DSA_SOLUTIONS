5f9eecdee95edb8d128a4aa20ec7600d91d82210
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-12 08:02:30 +0530

    Update 1665_Maximum_Initial_Energy_To_Finish_Tasks.java

--- Binary Search/1665_Maximum_Initial_Energy_To_Finish_Tasks.java
+++ Binary Search/1665_Maximum_Initial_Energy_To_Finish_Tasks.java
@@ -1,4 +1,13 @@
+// Question
+//You are given an array tasks where tasks[i] = [actuali, minimumi]:
 
+//actuali is the actual amount of energy you spend to finish the ith task.
+//minimumi is the minimum amount of energy you require to begin the ith task.
+//For example, if the task is [10, 12] and your current energy is 11, you cannot start this task. However, if your current energy is 13, you can complete this task, and your energy will be 3 after finishing it.
+
+//You can finish the tasks in any order you like.
+
+//Return the minimum initial amount of energy you will need to finish all the tasks.
 class Solution {
     private boolean check(int [][] tasks , int mid){
         int curr = mid;
@@ -41,3 +50,6 @@ class Solution {
         return ans;
     }
 }
+//Explanation 
+// We first sort the array in decreasing order of difference between required and actual energy this is because tasks with higher extra energy shall be performed
+// first. Then we perform binary search with initially lower bound as 0 and upper bound as sum of all required energies.
