ff7e164ee91dd2859b760bca7f99e8166de79f25
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-12 22:30:51 +0530

    Leetcode 368 solution

--- Dynamic Programming/368_Largest_Divisible_Subset.java
+++ Dynamic Programming/368_Largest_Divisible_Subset.java
@@ -1,3 +1,9 @@
+//Question
+//Given a set of distinct positive integers nums, 
+//return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+//answer[i] % answer[j] == 0, or
+//answer[j] % answer[i] == 0
+If there are multiple solutions, return any of them.
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
         Arrays.sort(nums);
