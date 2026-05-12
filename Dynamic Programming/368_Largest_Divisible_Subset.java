a49faa9637fda9b021008170b7555d670c454a26
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-12 22:29:17 +0530

    Update 368_Largest_Divisible_Subset.java

--- Dynamic Programming/368_Largest_Divisible_Subset.java
+++ Dynamic Programming/368_Largest_Divisible_Subset.java
@@ -1,1 +1,33 @@
-
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int [] dp = new int[n];
+        int [] next = new int[n];
+        for(int i = 0 ; i<n ; ++i) dp[i] = 1;
+        for(int i = 0 ; i<n ; ++i) next[i] = -1;
+        int ans = 0;
+        for(int i = n-1 ; i>=0 ; --i){
+            for(int j = i+1 ; j<n ; ++j){
+                if(nums[j]%nums[i] == 0 && 1+dp[j] > dp[i]){
+                    next[i] = j;
+                    dp[i] = 1+dp[j];
+                }
+            }
+            ans = Math.max(ans , dp[i]);
+        }
+        int idx = -1;
+        for(int i = 0 ; i<n ; ++i){
+            if(dp[i]==ans){
+                idx = i;
+                break;
+            }
+        }
+        List<Integer> l = new ArrayList<>();
+        while(idx!=-1){
+            l.add(nums[idx]);
+            idx = next[idx];
+        }
+        return l;
+    }
+}
