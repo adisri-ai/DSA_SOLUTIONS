1dffb6589724527d457840a2e7f5e44171cc13f0
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-12 18:07:00 +0530

    Update 301_Remove_Invalid_Parantheses.java

--- Graphs/301_Remove_Invalid_Parantheses.java
+++ Graphs/301_Remove_Invalid_Parantheses.java
@@ -1,3 +1,6 @@
+//Question
+//Given a string s that contains parentheses and letters, remove the minimum number of invalid parentheses to make the input string valid.
+//Return a list of unique strings that are valid with the minimum number of removals. You may return the answer in any order.
 class Solution {
     public boolean check(String s){
         int curr = 0;
@@ -45,3 +48,7 @@ class Solution {
         return final_ans;
     }
 }
+// Explanation 
+// For every string we eliminate one character either '(' or ')' if there is no answer found or if the length of the string is greater than the length of the ans
+// We perform BFS traversal using queue and upon finding a valid string when there was no ans found or if the length of ans was equal to the length of the string
+// we add it to the set. Finally, we add all the elements of the set to the string.
