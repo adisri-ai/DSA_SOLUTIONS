8d01c9a8990adfde596a50a080c221466e5ced83
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-14 22:13:27 +0530

    Create 518_Coin_Change_2.cpp

---
+++ Dynamic Programming/518_Coin_Change_2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size() , vector<int>(amount+1 , -1));
+        for(int i=0 ; i<dp.size() ; i++){
+            dp[i][0]=1;
+        }
+        for(int j=1 ; j<dp[0].size() ; j++){
+            if(j%coins[0]==0){
+                dp[0][j]=1;
+            }
+            else{
+                dp[0][j]=0;
+            }
+        }
+        for(int i=1 ; i<dp.size() ; i++){
+            for(int j=1; j<dp[0].size() ; j++){
+                int take = 0;
+                int ntake = dp[i-1][j];
+                if(coins[i]<=j) take = dp[i][j-coins[i]];
+                dp[i][j]= take+ntake;
+            }
+        }
+        return dp[dp.size()-1][amount];
+    }
+};
