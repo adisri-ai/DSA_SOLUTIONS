06f8bc912dcc0ee1080b51ec7b55e1b15765dfe0
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-17 21:42:31 +0530

    Update 1306_Jump_Game_III.java

--- BFS-DFS Traversal/1306_Jump_Game_III.java
+++ BFS-DFS Traversal/1306_Jump_Game_III.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        boolean[] vis = new boolean[n];
+        for(int i = 0 ; i<n ; ++i) vis[i] = false;
+        vis[start] = true;
+        while(!q.isEmpty()){
+            int x = q.poll();
+            if(arr[x]==0) return true;
+            if(x+arr[x]<n && !vis[x+arr[x]]){
+                vis[x+arr[x]] = true;
+                q.add(x+arr[x]);
+            }
+            if(x-arr[x]>=0 && !vis[x-arr[x]]){
+                vis[x-arr[x]] = true;
+                q.add(x-arr[x]);
+            }
+        }
+        return false;
+    }
+}
