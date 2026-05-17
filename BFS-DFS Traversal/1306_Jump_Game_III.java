610b86115af1d7a19503b0de6aad2603aa7d0001
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-17 21:43:24 +0530

    1306_Jump_Game_III.java

--- BFS-DFS Traversal/1306_Jump_Game_III.java
+++ BFS-DFS Traversal/1306_Jump_Game_III.java
@@ -1,3 +1,7 @@
+// Question
+//Given an array of non-negative integers arr, you are initially positioned at 
+//start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+//Notice that you can not jump outside of the array at any time.
 class Solution {
     public boolean canReach(int[] arr, int start) {
         int n = arr.length;
