5e22768c0067ae9f2dcc7f03fad309411f449d7c
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-05-28 20:58:32 +0530

    Create 421_Maximum_XOR_of_Twol_Elements.java

---
+++ Tries/421_Maximum_XOR_of_Twol_Elements.java
@@ -0,0 +1,103 @@
+// Question  
+//Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 <= i <= j < n.
+class Node {
+
+    Node[] arr;
+    boolean flag;
+
+    public Node() {
+        this.arr = new Node[2];
+        for (int i = 0; i < 2; ++i) {
+            this.arr[i] = null;
+        }
+        this.flag = false;
+    }
+}
+
+class Trie {
+
+    Node root;
+
+    public Trie() {
+        this.root = new Node();
+    }
+
+    public void insert(String k) {
+
+        Node curr = this.root;
+
+        for (int i = 0; i < 32; ++i) {
+
+            int c = k.charAt(i) - '0';
+
+            if (curr.arr[c] == null) {
+                curr.arr[c] = new Node();
+            }
+
+            curr = curr.arr[c];
+        }
+
+        curr.flag = true;
+    }
+
+    public int find_max(String k) {
+
+        Node curr = this.root;
+
+        int ans = 0;
+
+        for (int i = 0; i < 32; ++i) {
+
+            int bit = k.charAt(i) - '0';
+
+            int opposite = 1 - bit;
+
+            if (curr.arr[opposite] != null) {
+
+                ans = (ans << 1) | 1;
+
+                curr = curr.arr[opposite];
+            }
+            else {
+
+                ans = (ans << 1);
+
+                curr = curr.arr[bit];
+            }
+        }
+
+        return ans;
+    }
+}
+
+class Solution {
+
+    public int findMaximumXOR(int[] nums) {
+
+        Trie tr = new Trie();
+
+        for (int num : nums) {
+
+            String s = String.format(
+                    "%32s",
+                    Integer.toBinaryString(num)
+            ).replace(' ', '0');
+
+            tr.insert(s);
+        }
+
+        int ans = 0;
+
+        for (int num : nums) {
+
+            String s = String.format(
+                    "%32s",
+                    Integer.toBinaryString(num)
+            ).replace(' ', '0');
+
+            ans = Math.max(ans, tr.find_max(s));
+        }
+
+        return ans;
+    }
+}
